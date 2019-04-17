716226b34bdd5a75bf7e1bf92f5c4ec24944569c
brentkulwicki <37881163+brentkulwicki@users.noreply.github.com>
2019-04-17 06:16:07 -0400

    initial commit for dealer pro

---
+++ js/dealerPro.js
@@ -0,0 +1,66 @@
+const formName = document.getElementById('name');
+const formEmail = document.getElementById('email');
+const formPhone = document.getElementById('phone');
+const formComment = document.getElementById('comment');
+const formSubmit = document.getElementById('submit');
+
+const collectResponse = () => {
+    formName.removeAttribute('class', 'not-valid');
+    formEmail.removeAttribute('class', 'not-valid');
+    formPhone.removeAttribute('class', 'not-valid');
+    formComment.removeAttribute('class', 'not-valid');
+    let validName = false;
+    let validEmail = false;
+    let validPhone = false;
+    let validComment = false;
+    if (formName.value.length < 1) {
+        formName.setAttribute('class', 'not-valid');
+    } else {
+        validName = true;
+    };
+    if (formEmail.value.length < 5) {
+        formEmail.setAttribute('class', 'not-valid');
+    } else {
+        validEmail = true;
+    };
+    if (formPhone.value.length < 7) {
+        formPhone.setAttribute('class', 'not-valid');
+    } else {
+        validPhone = true;
+    };
+    if (formComment.value.length < 1) {
+        formComment.setAttribute('class', 'not-valid');
+    } else {
+        validComment = true;
+    };
+    if (validName === true && validEmail === true && validPhone === true && validComment === true) {
+        let responseObject = {
+            name: formName.value,
+            email: formEmail.value,
+            phone: formPhone.value,
+            comment: formComment.value,
+        };
+        responseObject = JSON.stringify(responseObject);
+        
+    };
+};
+
+const blurFunction = (e) => {
+    if (e.target.value.length < 1) {
+        e.target.setAttribute('class', 'not-valid');
+    };
+};
+
+const removeNVClass = (e) => {
+    e.target.removeAttribute('class', 'not-valid');
+};
+
+formName.addEventListener('blur', blurFunction);
+formEmail.addEventListener('blur', blurFunction);
+formPhone.addEventListener('blur', blurFunction);
+formComment.addEventListener('blur', blurFunction);
+formName.addEventListener('focus', removeNVClass);
+formEmail.addEventListener('focus', removeNVClass);
+formPhone.addEventListener('focus', removeNVClass);
+formComment.addEventListener('focus', removeNVClass);
+formSubmit.addEventListener('click', collectResponse);
